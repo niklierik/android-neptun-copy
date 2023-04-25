0a465831e03bb8e525e00a1fbdd2d06b92dcb6dc
niklierik <erik200203@gmail.com>
2023-04-25 16:24:41 +0200

    Added switch to register btn

--- app/src/main/java/me/eriknikli/neptuncopy/MainActivity.java
+++ app/src/main/java/me/eriknikli/neptuncopy/MainActivity.java
@@ -2,7 +2,9 @@ package me.eriknikli.neptuncopy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn = findViewById(R.id.login_btnToRegister);
+        btn.setOnClickListener((listener) -> {
+            Intent switchToRegister = new Intent(this, RegisterActivity.class);
+            startActivity(switchToRegister);
+        });
     }
 }
