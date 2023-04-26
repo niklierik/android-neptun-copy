e49245de3edc43e017706a8efb44ad79ddd1366e
niklierik <erik200203@gmail.com>
2023-04-26 13:54:12 +0200

    Register form

---
+++ app/src/main/java/me/eriknikli/neptuncopy/DateHandler.java
@@ -0,0 +1,21 @@
+package me.eriknikli.neptuncopy;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+// https://stackoverflow.com/questions/47252233/how-to-store-timestamp-on-firestore-using-android-edittext-and-datetimepicker-di
+public class DateHandler {
+
+    static final SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+
+    public static Date getDateFromString(String datetoSaved) {
+
+        try {
+            return format.parse(datetoSaved);
+        } catch (ParseException e) {
+            return null;
+        }
+
+    }
+}
